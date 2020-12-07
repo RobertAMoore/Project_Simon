9e285946d084b11e7049647a6aa30a9e95dd6a87
RobertAMoore <robertallenmooreiii@gmail.com>
2020-12-07 14:25:35 -0500

    Moved HTML files from Views to public/html

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,8 @@
 
 var express = require('express');
 var router = express.Router();
+//var userModel = require('../models/dbmodel');
+//var service = require('../services/services');
 var path = require('path');
 var bodyParser = require('body-parser');
 router.use(bodyParser.json()); // support json encoded bodies
@@ -14,7 +16,7 @@ router.get('/', function(req, res){
 
 //GET request for signup page
 router.get('/user/create', function(req, res){
-	res.sendFile(path.join(__dirname, '..', 'views', 'signUp.html'));
+	res.sendFile(path.join(__dirname, '..', 'public', 'html', 'signUp.html'));
 });
 
 //POST request to create new user
@@ -24,17 +26,21 @@ router.post('/user/create', function(req, res){
 
 //GET request for login page
 router.get('/user/login', function(req, res){
-	res.sendFile(path.join(__dirname, '..', 'views', 'login.html'));
+	res.sendFile(path.join(__dirname, '..', 'public', 'html', 'login.html'));
 });
 
 //POST request username and password to sign in
 router.post('/user/login', function(req, res){
-	res.redirect('/user/game');
+	//if (service.assertPass(req.body.username, req.body.password))
+	res.redirect('/user/game'); //Need to pass username
 });
 
 //GET request for game page
 router.get('/user/game', function(req, res){
-	res.sendFile(path.join(__dirname, '..', 'views', 'game.html'));
+	//res.sendFile(path.join(__dirname, '..', 'public', 'html', 'game.html'));
+	res.locals.user=req.body.username; //We can make this dynamic later
+	req.session.valid = true;
+	res.render('layout');
 });
 
 //GET current high score
@@ -44,7 +50,7 @@ router.get('/user/:username/highScore', function(req, res){
 
 //POST new score /user/:username/new-score
 router.post('/user/newScore', function(req, res){
-	console.log(req.body);
+	console.log(req.body.score);
 	res.send(`NOT IMPLEMENTED: user score POST`);
 });
 
